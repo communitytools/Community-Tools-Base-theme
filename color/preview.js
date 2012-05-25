ec5a25c75be34d8920628b767112f250d590555e
Reigo <reigok@gmail.com>
2012-05-25 13:47:03 +0300

    Lots of changes

--- color/preview.js
+++ color/preview.js
@@ -16,18 +16,19 @@
       }
       */
 
-      $('#preview-header, #preview-logo-area', form).css('background-color', $('#palette input[name="palette[base]"]', form).val());
+      $('#preview-header, #preview-logo-area, #preview-menu', form).css('background-color', $('#palette input[name="palette[base]"]', form).val());
       $('#preview-content a', form).css('color', $('#palette input[name="palette[link]"]', form).val());
 
       $('#preview-menu li a.active', form).css('border-color', $('#palette input[name="palette[link]"]', form).val());
 
+      $('#preview-site-slogan', form).css('color', $('#palette input[name="palette[slogan]"]', form).val());
 
       $('#preview-content, #preview-footer', form).css('color', $('#palette input[name="palette[text]"]', form).val());
 
       $('#preview-button', form).css('background-color', $('#palette input[name="palette[button]"]', form).val());
 
       $('#preview-footer', form).css('background-color', $('#palette input[name="palette[footer]"]', form).val());
-      $('#preview-footer a', form).css('color', $('#palette input[name="palette[footer_links]"]', form).val());
+      $('#preview-footer a', form).css('color', $('#palette input[name="palette[footerlinks]"]', form).val());
 
 
     }
